9224cd43bd463205d3e3b952a88806e8580d5bb8
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-11-18 20:36:04 -0800

    cleaned up API file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,10 +4,10 @@ export default {
   getSports: () => {
     // const key = process.env.REACT_APP_SPORTSBOOK_API_KEY;
     // console.log(key);
-    return axios.get(`https://api.the-odds-api.com/v3/sports?apiKey=b1b145ddd8b89c13687841bd27fab349`);
+    return axios.get(`https://api.the-odds-api.com/v3/sports?apiKey=`);
   },
 
   getLines: (sport) => {
-    return axios.get(`https://api.the-odds-api.com/v3/odds?&apiKey=b1b145ddd8b89c13687841bd27fab349&sport=${ sport }&region=us&mkt=h2h&oddsFormat=american`);
+    return axios.get(`https://api.the-odds-api.com/v3/odds?&apiKey=&sport=${ sport }&region=us&mkt=h2h&oddsFormat=american`);
   }
 };
